4f1718ad743de9e0cd412a39f20ca21711367be1
gabrielhayoun <78904699+gabrielhayoun@users.noreply.github.com>
2021-05-17 20:03:04 +0200

    mise en place des pages login et logout.

--- projet/communitymanager/models.py
+++ projet/communitymanager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=100)
+    subscribers=models.ManyToManyField(User)
+
+    class Meta:
+        verbose_name = "communitie"
+        ordering = ['name']
--- projet/communitymanager/urls.py
+++ projet/communitymanager/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
-
+    path('accounts/', include('django.contrib.auth.urls')),
     ]
